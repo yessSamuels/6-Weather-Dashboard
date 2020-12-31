17d1ab3187f2f62a3d91a7bbcdafddb63d44a66c
Yessica Samuels <yessicaperez@me.com>
2020-12-31 12:53:01 -0800

    new

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = document.querySelector("className");
+
+const notificationElement = document.querySelector("notification");
+const iconElement = document.querySelector("weather-icon");
+const tempElement = document.querySelector("temperature-value p");
+const notificationElement = document.querySelector(".temperature-descriptiom p");
+const notificationElement = document.querySelector("location p");
+
+
+iconElement.innerHTML =
+
+tempElement.innerHTML = 
+
+locationElement.innerHTML = 
